4528ca00a28152a541756cad514fa84e318f50a9
borislavkostov <borislavkostov97@gmail.com>
2017-02-28 20:36:11 +0200

    working with JAXB

---
+++ JavaApplication17/src/Trash.java
@@ -0,0 +1,27 @@
+
+
+
+
+
+import com.oracle.jrockit.jfr.Transition;
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import weather.*;
+public class Trash {
+    public static void main(String args[]) throws JAXBException{
+        String url = "http://api.openweathermap.org/data/2.5/weather?id=727011&cnt=10&appid=7330bff2928c454b01b3d2f40a5f2709&units=metric&mode=xml";
+        JAXBContext jc = JAXBContext.newInstance(Current.class);
+
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        File xml = new File("/home/templars1914/customer.xml");
+        Current currWeather = (Current) unmarshaller.unmarshal(xml);
+        System.out.println(currWeather);
+        Marshaller marshaller = jc.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(currWeather, System.out);
+
+    }
+}
