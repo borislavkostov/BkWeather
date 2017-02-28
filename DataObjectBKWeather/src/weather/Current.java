b4df164b117f2f0b68c07bb948699d664d0afe4e
borislavkostov <borislavkostov97@gmail.com>
2017-02-28 21:08:32 +0200

    working with JAXB

---
+++ DataObjectBKWeather/src/weather/Current.java
@@ -0,0 +1,42 @@
+package weather;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Current {
+
+    private City city;
+    private Temperature temperature;
+    private Weather weather;
+
+    public void setWeather(Weather weather) {
+        this.weather = weather;
+    }
+
+    public Weather getWeather() {
+        return weather;
+    }
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    @XmlElement
+    public void setTemperature(Temperature temperature) {
+        this.temperature = temperature;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public Temperature getTemperature() {
+        return temperature;
+    }
+
+    @Override
+    public String toString() {
+        return city.getName() + city.getCountry() + temperature.getValue(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ DataObjectBKWeather/src/weather/Temperature.java
@@ -0,0 +1,39 @@
+package weather;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+public class Temperature {
+
+    private double value;
+    private double min;
+    private double max;
+
+    public double getMax() {
+        return max;
+    }
+
+    public double getMin() {
+        return min;
+    }
+    @XmlAttribute
+    public void setMax(double max) {
+        this.max = max;
+    }
+    @XmlAttribute
+    public void setMin(double min) {
+        this.min = min;
+    }
+
+    public Temperature() {
+    }
+    @XmlAttribute
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+}
